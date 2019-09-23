d723dfc24d8706d3f971c17f9f4351a276f2d60f
thulanirex <rexroony@gmail.com>
2019-09-23 15:31:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// *************************************************************************
+// *                                                                       *
+// * iBilling -  Accounting, Billing Software                              *
+// * Copyright (c) Sadia Sharmin. All Rights Reserved                      *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * Email: sadiasharmin3139@gmail.com                                     *
+// * Website: http://www.sadiasharmin.com                                  *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * This software is furnished under a license and may be used and copied *
+// * only  in  accordance  with  the  terms  of such  license and with the *
+// * inclusion of the above copyright notice.                              *
+// * If you Purchased from Codecanyon, Please read the full License from   *
+// * here- http://codecanyon.net/licenses/standard                         *
+// *                                                                       *
+// *************************************************************************
+
+
+define('APP_RUN', true);
+require ('application/app.php');
