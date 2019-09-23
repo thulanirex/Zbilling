e6f63c82b031ffe012225787414d82fc74fdb1a9
thulanirex <rexroony@gmail.com>
2019-09-23 15:38:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// *************************************************************************
+// *                                                                       *
+// * ZBilling -  Accounting, Billing Software                              *
+// * Copyright (c) Thulani Rex. All Rights Reserved                      *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * Email: thulanirex@gmail.com                                     *
+// * Website: http://www.thulanirex.com                                  *
+// *                                                                       *
+// *************************************************************************
+// *                                                                       *
+// * This software is furnished under a license and may be used and copied *
+// * only  in  accordance  with  the  terms  of such  license and with the *
+// * inclusion of the above copyright notice.                              *                       *
+// *                                                                       *
+// *************************************************************************
+
+
+define('APP_RUN', true);
+require ('application/app.php');
